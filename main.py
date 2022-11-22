49120049e27cba134361078d3593cc0dc0ad6613
Anders <andersob@ifi.uio.no>
2022-11-22 23:17:50 +0100

    Initial version

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
 import discord
+import asyncio
+import re
 
 
 class MyClient(discord.Client):
-    pass
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+        channels = self.get_all_channels()
+
+        for channel in channels:
+            if channel.category:
+                print(channel)
+                messages = await channel.history(limit=10).flatten()
+                for message in messages:
+                    reactions = [reaction for reaction in message.reactions if isinstance(reaction.emoji, discord.Emoji)]
+                    for reaction in reactions:
+                        print(reaction.emoji)
+
+                    custom_emojis = re.findall(r'<:\w*:\d*>', message.content)
+                    custom_emojis = [int(e.split(':')[2].replace('>', '')) for e in custom_emojis]
+                    custom_emojis = [self.get_emoji(e) for e in custom_emojis]
+                    print(custom_emojis)
+
+        await self.close()
 
 
+def main():
+    import os
+    from dotenv import load_dotenv
+    load_dotenv()
+    discord_token = os.getenv('TOKEN')
+
+    client = MyClient()
+    client.run(discord_token)
 
 
 if __name__ == "__main__":
-    import os
-    discord_token = os.getenv('TOKEN')
+    main()
--- makefile
+++ makefile
@@ -1,5 +1,7 @@
-all:
+run:
 		python3 main.py
+
+all: dependencies run
 
 dependencies:
 		pip3 install -r dependencies.txt
